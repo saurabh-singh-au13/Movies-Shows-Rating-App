820c99d280f138e71a5aa6d48ba9e60652869aca
Saurabh Kumar Singh <s.rajvansh.1996@gmail.com>
2023-04-13 17:09:22 +0530

    added live url

--- src/common/apis/movieApi.js
+++ src/common/apis/movieApi.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://www.omdbapi.com"
+    // baseURL: "http://www.omdbapi.com"
+    baseURL: "https://movierating-5171b.web.app"
 })
