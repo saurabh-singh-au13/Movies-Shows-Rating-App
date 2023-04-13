5762eea8dbd735d98c7489e7d9047c18c300d034
Saurabh Kumar Singh <s.rajvansh.1996@gmail.com>
2023-04-13 17:56:50 +0530

    https

--- src/common/apis/movieApi.js
+++ src/common/apis/movieApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: "http://www.omdbapi.com"
-    baseURL: "https://movierating-5171b.web.app"
+    baseURL: "https://www.omdbapi.com"
+    // baseURL: "https://movierating-5171b.web.app"
 })
